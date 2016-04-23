47d0028a3006ce9b702f97cf6e35ab8fa7401c3f
daoffner <dpoffner@sbcglobal.net>
2016-04-23 18:19:36 -0500

    Added a lobster button, classes for buildings, and a click counter
    The building classes can generate the price to buy, and holds the values
    for a building. The values are base cost, rate of lobsters, and number
    of buildings.

--- app/src/main/java/com/example/david/lobster_clicker/Game.java
+++ app/src/main/java/com/example/david/lobster_clicker/Game.java
@@ -4,15 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Game extends AppCompatActivity {
     private Button homeButton;
+    private Button lobster;
+    private int lobsterCount=0;
+    private TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
+        lobster= (Button) findViewById((R.id.lobster));
+        lobster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lobsterCount++;
+                display= (TextView) findViewById(R.id.countDisplay);
+                display.setText(lobsterCount+" Lobsters");
+            }
+        });
         homeButton = (Button) findViewById(R.id.home);
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
